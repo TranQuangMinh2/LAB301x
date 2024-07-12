14cef934adfb50f56ce3f9ffa1ca85aabc4ca364
TranQuangMinh2 <90443659+TranQuangMinh2@users.noreply.github.com>
2024-07-12 08:06:00 +0700

    Delete ResourceNotFoundException.java

--- ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.trnqngmnh.library;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
