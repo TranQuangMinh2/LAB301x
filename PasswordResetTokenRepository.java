835f5092540af551238cd1119f85647a14d91761
TranQuangMinh2 <90443659+TranQuangMinh2@users.noreply.github.com>
2024-07-12 07:43:32 +0700

    Revert "Add files via upload"

--- PasswordResetTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.trnqngmnh.library;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
-	PasswordResetToken findByToken(String token);
-}
