3de905c4be9e7b1b242909ecd38a68cf82c2b3ee
TranQuangMinh2 <90443659+TranQuangMinh2@users.noreply.github.com>
2025-06-02 15:53:19 +0700

    Delete java/ProductSizeRepository.java

--- java/ProductSizeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.trnqngmnh.library;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductSizeRepository extends JpaRepository<ProductSize, Long> {
-}
