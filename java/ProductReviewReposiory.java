1b24be61457e812fdb4783b103f702c15c5a9508
TranQuangMinh2 <90443659+TranQuangMinh2@users.noreply.github.com>
2025-06-02 15:52:30 +0700

    Delete java/ProductReviewReposiory.java

--- java/ProductReviewReposiory.java
+++
@@ -1,9 +0,0 @@
-package com.trnqngmnh.library;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductReviewReposiory extends JpaRepository<ProductReview, Long> {
-
-}
